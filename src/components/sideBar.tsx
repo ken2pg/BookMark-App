f841595388061288ff073f340ef5547799a088b3
kento <jst34zais9102z@gmail.com>
2020-08-22 18:58:06 +0900

    start,end editの追加

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
+import Dialog, { DialogProps } from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
@@ -26,6 +32,8 @@ import FolderItem from './folderItem';
 
 import FolderIcon from '@material-ui/icons/Folder';
 
+import { folder } from '#/types/folder';
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -44,15 +52,45 @@ const useStyles = makeStyles((theme: Theme) =>
       width: 240,
       zIndex: -100,
     },
+    button: {},
   })
 );
 
 const SideBar = () => {
   const classes = useStyles();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const state = useSelector((state: RootState) => state);
+  const [fullWidth, setFullWidth] = React.useState(true);
+  const [maxWidth, setMaxWidth] = React.useState<DialogProps['maxWidth']>('sm');
 
-  //   console.log(state.sideBar.folder);
+  const startEdit = () => {
+    dispatch(sideBarSlice.actions.startEditFolder());
+  };
+
+  const cancelEdit = () => {
+    dispatch(sideBarSlice.actions.endEditFolder());
+  };
+  const addFolder = (folder: folder) => {
+    dispatch(sideBarSlice.actions.addFolder(folder));
+  };
+
+  const EditMenu = (
+    <div>
+      <Dialog
+        fullWidth={fullWidth}
+        maxWidth={maxWidth}
+        open={state.sideBar.folder.isEdit}
+        onClose={cancelEdit}
+        aria-labelledby="max-width-dialog-title"
+      >
+        <DialogTitle id="max-width-dialog-title"> Create New Folder</DialogTitle>
+        <DialogContent>
+          <DialogContentText>test</DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+  console.log(state.sideBar.saveFolder);
   return (
     <div className={classes.root}>
       <Drawer
@@ -67,21 +105,22 @@ const SideBar = () => {
           Folder List
         </Typography>
         <Divider />
+        <Button className={classes.button} variant="outlined" color="primary" onClick={startEdit}>
+          Create New Folder
+        </Button>
         <List>
-          <ListItem button key={'sample1'}>
-            <ListItemIcon>
-              <FolderIcon></FolderIcon>
-            </ListItemIcon>
-            <FolderItem />
-          </ListItem>
-          <ListItem button key={'sample1'}>
-            <ListItemIcon>
-              <FolderIcon></FolderIcon>
-            </ListItemIcon>
-            <FolderItem />
-          </ListItem>
+          {state.sideBar.saveFolder.map((folder, _i) => {
+            <ListItem button key={'sample1'}>
+              <ListItemIcon>
+                <FolderIcon></FolderIcon>
+              </ListItemIcon>
+              <FolderItem folder={state.sideBar.folder} />
+            </ListItem>;
+          })}
         </List>
       </Drawer>
+      {/*新規フォルダー作成画面*/}
+      {EditMenu}
     </div>
   );
 };
