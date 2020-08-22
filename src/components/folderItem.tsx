888fdd1660857fe841cc87cbf4e6f2ea69b3b8ad
kento <jst34zais9102z@gmail.com>
2020-08-22 18:53:44 +0900

    Propsの追加

--- src/components/folderItem.tsx
+++ src/components/folderItem.tsx
@@ -2,27 +2,6 @@ import React from 'react';
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-
-import { sideBarSlice } from '../slices/sideBarSlice';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -36,13 +15,21 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-const FolderItem = () => {
-  //   const classes = useStyles();
-  //   const dispatch = useDispatch();
-  //   const state = useSelector((state: RootState) => state);
+interface Props {
+  folder: {
+    userId: number;
+    folderId: number;
+    folderName: string;
+    folderColor: string;
+    isEdit: boolean;
+  };
+}
+
+const FolderItem: React.FC<Props> = ({ folder }) => {
+  const classes = useStyles();
   return (
     <div>
-      <p>test1</p>
+      <p>{folder.folderName}</p>
     </div>
   );
 };
