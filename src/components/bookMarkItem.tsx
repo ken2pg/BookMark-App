16280a279e24728bd1dc6cc784d5924766ef54a8
kento <jst34zais9102z@gmail.com>
2020-09-01 21:23:50 +0900

    store内のstateが表示されるように変更

--- src/components/bookMarkItem.tsx
+++ src/components/bookMarkItem.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
@@ -8,6 +12,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { bookMarkSlice } from '#/slices/bookMarkSlice';
 
 const useStyle = makeStyles((theme: Theme) =>
   createStyles({
@@ -46,7 +51,12 @@ interface Props {
 }
 const BookMarkItem: React.FC<Props> = ({ bookMarkContents }) => {
   const classes = useStyle();
+  const dispatch = useDispatch();
+  const state = useSelector((state: RootState) => state);
 
+  const startEditBookMark = () => {
+    dispatch(bookMarkSlice.actions.startEditBookMark());
+  };
   //   const text: string = 'http://www.google.com/s2/favicons?domain=https://github.com';
   //   const text: string = 'http://www.yahoo.co.jp/aaa.jpg';
   return (
@@ -58,15 +68,15 @@ const BookMarkItem: React.FC<Props> = ({ bookMarkContents }) => {
         <CardContent className={classes.cardContent}>
           <Typography className={classes.name}>{bookMarkContents.siteName}</Typography>
           <Typography color="textSecondary" className={classes.text}>
-            {bookMarkContents.siteURL}
+            URL {bookMarkContents.siteURL}
           </Typography>
           <Typography color="textSecondary" className={classes.text}>
-            {bookMarkContents.date}
+            Date {bookMarkContents.date}
           </Typography>
         </CardContent>
         {/* </CardActionArea> */}
         <Button color="primary" className={classes.btn}>
-          Detail
+          Memo
         </Button>
         <Button color="primary" className={classes.btn}>
           Edit
