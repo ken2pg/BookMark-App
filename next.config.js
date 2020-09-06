1f53b8c3712169c40448296e9f90132364720b41
kento <jst34zais9102z@gmail.com>
2020-09-06 12:45:09 +0900

    ファイルを追加

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// require('dotenv').config()
+
+module.exports = {
+  env: {
+    TEST_TEXT: process.env.TEST_TEXT,
+  },
+  publicRuntimeConfig: {
+    TEST_TEXT: process.env.TEST_TEXT,
+  },
+};
