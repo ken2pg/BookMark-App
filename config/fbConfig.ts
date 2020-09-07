7117e7b37bd5a6a59827a3a383ea1c26a3781dd3
kento <jst34zais9102z@gmail.com>
2020-09-07 17:30:53 +0900

    firebase.initializeAppでバグが生じたので、初回以外初期化しないように変更

--- config/fbConfig.ts
+++ config/fbConfig.ts
@@ -12,6 +12,8 @@ const firebaseConfig = {
   measurementId: process.env.MEASUREMENT_ID,
 };
 
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 export const firebaseStore = firebase.firestore();
 export default firebase;
