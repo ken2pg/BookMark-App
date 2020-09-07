3cb8de4abb4d847362afd4cf2e154f1d215f891b
kento <jst34zais9102z@gmail.com>
2020-09-07 17:31:50 +0900

    環境変数を取得するコードを追加

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-// require('dotenv').config()
+require('dotenv').config();
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 module.exports = {
   env: {
-    TEST_TEXT: process.env.TEST_TEXT,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORANGE_BUCKET: process.env.STORANGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
   },
-  publicRuntimeConfig: {
-    TEST_TEXT: process.env.TEST_TEXT,
-  },
+  publicRuntimeConfig: {},
 };
