7edf4db7becd14e5e63daf1038cf8d4a7f71373f
kento <jst34zais9102z@gmail.com>
2020-09-07 17:17:56 +0900

    envファイル(開発時)から各値を取得するように変更

---
+++ config/fbConfig.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORANGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firebaseStore = firebase.firestore();
+export default firebase;
