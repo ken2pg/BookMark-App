1e2d0e6e613b761cc9d07d07406f89f269413f19
kento <jst34zais9102z@gmail.com>
2020-09-14 19:02:58 +0900

    index.tsxの内容をhome.tsxに移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,69 +5,23 @@ import Typography from '@material-ui/core/Typography';
 import Router from 'next/router';
 import NavigationBar from '../components/navigationBar';
 import BookMarkComponent from '../components/bookMarkComponent';
+import Home from '#/components/home';
+
+import { NextPage } from 'next';
+import { withRouter } from 'next/router';
+import { WithRouterProps } from 'next/dist/client/with-router';
 
 const useStyle = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      width: '45%',
-      margin: '180px 8%',
-      textAlign: 'left',
-    },
-    title: {
-      // margin: '200px,auto',
-      fontSize: '44px',
-      fontWeight: theme.typography.fontWeightBold,
-      marginBottom: '50px',
-    },
-    description: {
-      fontSize: '20px',
-      marginBottom: '40px',
-      fontWeight: theme.typography.fontWeightMedium,
-    },
-    button: {
-      marginRight: '40px',
-      fontWeight: theme.typography.fontWeightBold,
-    },
+    root: {},
   })
 );
 
 const Index = () => {
-  const classes = useStyle();
   return (
     <>
       <NavigationBar />
-      <div className={classes.root}>
-        <Typography color={'primary'} className={classes.title}>
-          {/* Online BookMark Application for PC, Tablet, and Mobile */}
-          PC, タブレット, モバイルで使えるオンラインブックマークアプリ
-        </Typography>
-        <Typography color={'primary'} className={classes.description}>
-          {/* You can save your favorite site's URL and it can be shared and used by your PC, tablet and
-          mobile */}
-          ・お気に入りのサイトURLを保存することができ、いつでもすぐにブックマークアプリからアクセスすることができます。
-        </Typography>
-        <Typography color={'primary'} className={classes.description}>
-          ・またPC, モバイル、タブレット間でブックマークを共有することができます。
-        </Typography>
-        <Typography color={'primary'} className={classes.description}>
-          ・メモ機能がついており、大事な部分をメモできます
-        </Typography>
-        <div>
-          <Button
-            className={classes.button}
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              Router.push({ pathname: './signIn' });
-            }}
-          >
-            サインイン
-          </Button>
-          {/* <Button className={classes.button} variant="contained" color="primary">
-            アカウント登録
-          </Button> */}
-        </div>
-      </div>
+      <Home />
     </>
   );
 };
